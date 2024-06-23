c7dbbc7d1ef16bd5435a576a7045e5698e43019b
jayadeep11 <jayadeep.bellamkonda@gmail.com>
2024-06-23 16:58:03 +0530

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id='Notes' className='mt-12'>
-      <div className='lg:w-1/2 w-full'>
-        <h1 className=' text-center lg:text-start md:text-9xl text-6xl lg:text-[13rem] im'>Lets work together</h1>
+    <div id='Notes' className='mt-12 flex flex-col lg:flex-row lg:justify-between '>
+      <div className='lg:w-1/2 w-full '>
+        <h1 className=' text-center lg:text-start md:text-9xl text-6xl lg:text-[12rem] im'>Lets work together</h1>
+      </div>
+      <div className='lg:w-2/6 border border-neutral-700 rounded-lg flex mt-12 flex-col justify-around gap-5 p-10 lg:px-10 '>
+        <div className='flex flex-col gap-5 '>
+          <label className='font-bold text-neutral-700'>Name</label>
+          <input type='text' className='bg-transparent border-b border-neutral-700 focus:outline-none focus:border-white ' />
+          <label className='font-bold text-neutral-700'>email</label>
+          <input type='text' className='bg-transparent border-b border-neutral-700 focus:outline-none focus:border-white' />
+          <label className='group font-bold text-neutral-700'>mobile</label>
+          <input type='text' className='bg-transparent group-focus:text-white  border-b border-neutral-700 focus:outline-none focus:border-white' />
+        </div>
+        <button className='border border-neutral-700 p-4 rounded-lg hover:bg-white hover:text-black'>Submit</button>
       </div>
 
     </div >
@@ -12,4 +23,6 @@ const Contact = () => {
 }
 
 export default Contact
+
+
 
