50778f8b5a9f786d7a3d95a58bce4bb043e7e336
jayadeep11 <jayadeep.bellamkonda@gmail.com>
2024-07-03 17:26:11 +0530

    ...

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id='Notes' className='mt-12 flex flex-col lg:flex-row lg:justify-between '>
+    <div id='Notes' className='flex flex-col lg:flex-row items-center lg:justify-between h-screen '>
       <div className='lg:w-1/2 w-full '>
         <h1 className=' text-center lg:text-start font-acorn  md:text-9xl text-6xl lg:text-[12rem] im'>Lets work together</h1>
       </div>
-      <div className='lg:w-2/6 border border-neutral-700 rounded-lg flex mt-12 flex-col justify-around gap-5 p-10 lg:px-10 '>
+      <div className='hidden lg:w-2/6 border border-neutral-700 rounded-lg flex mt-12 flex-col justify-around gap-5 p-10 lg:px-10 '>
         <div className='flex flex-col gap-5 '>
           <label className='font-bold text-neutral-700'>Name</label>
           <input type='text' className='bg-transparent border-b border-neutral-700 focus:outline-none focus:border-white ' />
